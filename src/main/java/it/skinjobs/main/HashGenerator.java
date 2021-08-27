52825d025bbac302022812438a90f4dfac78f85d
jessica <jessica.vecchia@gmail.com>
2021-08-27 19:30:25 +0200

    date per hash

--- src/main/java/it/skinjobs/main/HashGenerator.java
+++ src/main/java/it/skinjobs/main/HashGenerator.java
@@ -2,10 +2,13 @@ package it.skinjobs.main;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.util.Date;
 
 public class HashGenerator {
 
     public static String hash(String message) throws Exception{
+        String timeStamp = " - " + new Date().getTime();
+        message = timeStamp + message;
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         byte[] hash = digest.digest(message.getBytes(StandardCharsets.UTF_8));
         StringBuilder hexString = new StringBuilder(2 * hash.length);
